e3dbe5a7979cf630e890e1f8c486b6eb1d91398f
Marlon Hernández <mmarlon.ssantiago@gmail.com>
2024-08-27 20:01:11 -0500

    Deleted trash code.

--- preload.js
+++ preload.js
@@ -6,76 +6,7 @@ const path = require("path");
 const { spawn } = require("child_process");
 const { rejects } = require("assert");
 
-const modules = {
-  katmodules: {
-    moduleFolderPath: path.join(__dirname, "modules"),
-    loadModules: function (modules, loadedPreloads) {
-      return new Promise((resolve, reject) => {
-        try {
-          this.moduleFolders = fs.readdirSync(this.moduleFolderPath);
-          const moduleFolderPath = this.moduleFolderPath;
-          const moduleFolders = this.moduleFolders;
-
-          const validModules = [];
-
-          moduleFolders.forEach((folder) => {
-            const folderPath = path.join(moduleFolderPath, folder);
-            const filesInFolder = fs.readdirSync(folderPath);
-
-            // Buscar archivo main en la carpeta
-            const mainFile = filesInFolder.find((file) =>
-              ["main.js", "main.bat", "main.py"].includes(file.toLowerCase())
-            );
-
-            if (mainFile) {
-              const filePath = path.join(folderPath, mainFile);
-              const extname = path.extname(mainFile);
-
-              // Solo cargar archivos con extensiones .js
-              if (extname === ".js") {
-                validModules.push({
-                  folder: folder,
-                  file: mainFile,
-                  path: filePath,
-                  type: extname,
-                });
-
-                const scriptContent = fs.readFileSync(filePath, "utf-8");
-                const moduleWrapper = (function (loadedPreloads) {
-                  return function (modules) {
-                    eval(scriptContent);
-                  };
-                })(modules, loadedPreloads, __dirname);
-
-                moduleWrapper(modules);
-              } else if (extname === ".bat" || extname === ".py") {
-                // Solo advertir sobre archivos bat y py
-                console.warn(`Archivo ${extname} encontrado: ${filePath}`);
-                validModules.push({
-                  folder: folder,
-                  file: mainFile,
-                  path: filePath,
-                  type: extname,
-                });
-              }
-            }
-          });
-
-          this.validModules = validModules;
-          resolve(); // Opcional: puedes pasar algún valor como resolve(valor)
-        } catch (err) {
-          reject(err);
-        }
-      });
-    },
-    getModules: function () {
-      return this.validModules || [];
-    },
-  },
-};
-
 // Cargar solo módulos válidos
-
 contextBridge.exposeInMainWorld("electron", {
   runCmd: (command) => ipcRenderer.send("run-cmd", command),
   onCmdOutput: (callback) =>
@@ -97,5 +28,5 @@ contextBridge.exposeInMainWorld("electron", {
   },
   send: (channel, data) => ipcRenderer.send(channel, data),
   on: (channel, callback) =>
-    ipcRenderer.on(channel, (event, ...args) => callback(...args)),
+    ipcRenderer.on(channel, (event, ...args) => callback(...args))
 });
